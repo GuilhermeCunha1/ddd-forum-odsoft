9e58a21b98724b16fc6156a656f31bfb12b6dc5f
pedrocosta <1211439@isep.ipp.pt>
2024-10-03 23:24:52 +0100

    Update jest

--- jest.config.js
+++ jest.config.js
@@ -3,15 +3,25 @@ module.exports = {
     '^.+\\.ts?$': 'ts-jest'
   },
   testEnvironment: 'node',
-  testRegex: './src/.*\\.(test|spec)?\\.(ts|ts)$',
+  testRegex: './src/.*\\.(test|spec)\\.ts$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   "roots": [
     "<rootDir>/src"
-  ], 
+  ],
   reporters: [
     "default",
-    ["./node_modules/jest-html-reporter", {
-      "pageTitle": "Test Report"
-    }]
-  ],
-};
+    [
+      "jest-html-reporter",
+      {
+        pageTitle: "Test Report",
+        outputPath: "./test-report.html"
+      }
+    ],
+    [
+      "jest-junit",
+      {
+        outputDirectory: "./test-report",
+        outputName: "junit.xml"
+      }
+    ]
+  ]};

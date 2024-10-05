86cab905e0904c5ccda0a5369d87355fb70f05b7
pedrocosta <1211439@isep.ipp.pt>
2024-10-05 14:27:43 +0100

    Update repository

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   testEnvironment: 'node',
   testRegex: './src/.*\\.(test|spec)\\.ts$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  "roots": [
+  roots: [
     "<rootDir>/src"
   ],
   reporters: [
@@ -14,14 +14,23 @@ module.exports = {
       "jest-html-reporter",
       {
         pageTitle: "Test Report",
-        outputPath: "./test-report.html"
+        outputPath: "./reports/test-report.html", // Ajustado o caminho para uma pasta específica
+        includeFailureMsg: true, // Incluir mensagens de falha no relatório
+        includeSuiteFailure: true, // Incluir falha de toda a suíte
+        useBrowserName: false // Defina como true se quiser diferenciar por navegador
       }
     ],
     [
       "jest-junit",
       {
-        outputDirectory: "./test-report",
-        outputName: "junit.xml"
+        outputDirectory: "./reports", // Diretório onde será salvo o relatório XML
+        outputName: "junit.xml", // Nome do arquivo XML
+        suiteName: "jest tests", // Nome da suíte de testes
+        classNameTemplate: "{classname}-{title}", // Formato da classe no relatório
+        titleTemplate: "{classname}-{title}", // Formato do título no relatório
+        ancestorSeparator: " › ", // Separador para nomes ancestrais
+        usePathForSuiteName: "true" // Usar o caminho do arquivo para o nome da suíte
       }
     ]
-  ]};
+  ]
+};
